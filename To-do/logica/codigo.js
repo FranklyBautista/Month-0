0023f411fc62c269b1850c36cf6e563643f0a6e8
FranklyBautista <franklybautista0511@gmail.com>
2025-08-29 20:40:51 -0400

    Implementacion del modelo basico de to-do list hace un basicamente un CRUD pero aun no guarda los datos

--- To-do/logica/codigo.js
+++ To-do/logica/codigo.js
@@ -1,7 +1,41 @@
-const inpuTexto = document.getElementById("taskInput")
-const btnAgregar = document.getElementById("addBtn")
+const inpuTexto = document.getElementById("taskInput");
+const btnAgregar = document.getElementById("addBtn");
+const tasksSpace = document.querySelector(".task-list");
+const btnComplete = document.querySelector(".cmplBtn");
 
-btnAgregar.addEventListener("click",()=>{
-    const texto = inpuTexto.value.trim();
-    alert(texto)
-})
+//Agregar Tarea
+btnAgregar.addEventListener("click", () => {
+  const texto = inpuTexto.value.trim();
+  if (texto !== "") {
+    tasksSpace.innerHTML += `
+            <li class="task-item">
+                <span class="task">${texto}</span>
+                <button class="dltBtn">Delete</button>
+                <button class="cmplBtn">Complete</button>
+                <button class="edBtn">Edit</button>
+            </li>`
+
+    
+    inpuTexto.value = "";
+  }else{
+    alert("No puede agregar una tarea vacia")
+  }
+});
+
+
+
+tasksSpace.addEventListener("click", (e) => {
+    //Marca Tarea como completada
+  if (e.target.classList.contains("cmplBtn")) {
+    const task = e.target.parentElement.querySelector(".task");
+    task.style.textDecoration = "line-through";
+    
+  }
+
+  //Remover Tarea
+  if(e.target.classList.contains("dltBtn")){
+    e.target.parentElement.remove();
+  }
+});
+
+
